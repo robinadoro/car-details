26f49fc3f9a9e0e5739ab7c15d181995725d7bdb
Robinson Idagiza <robinadoro@gmail.com>
2022-04-22 11:55:13 +0300

    ft:Add function to clear clear form

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-
-    function myFunction() {
-        alert("Your message is received")
-    }
-    document.querySelector('form').onsubmit = e => {
-        e.target.submit();
-        e.target.reset();
-        return false;
-    };
+function myFunction() {
+    alert("Your message is received, we will get back to you")
+}
+document.querySelector('#contact_form').onsubmit = e => {
+    e.target.submit();
+    e.target.reset();
+    return false;
+};
+// document.getElementById("contact_form").reset();
